4067e92db06e0164bda006ee6f2e9ae781370b11
Mark Tranter <mark.a.tranter@gmail.com>
2021-06-24 20:57:52 +1000

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
-const merge = require('merge')
-const tsPreset = require('ts-jest/jest-preset')
-const dynamoPreset = require("@shelf/jest-dynamodb/jest-preset")
+const merge = require('merge');
+const tsPreset = require('ts-jest/jest-preset');
+const dynamoPreset = require('@shelf/jest-dynamodb/jest-preset');
 
 const config = merge(tsPreset, dynamoPreset, {
   testEnvironment: 'node',
-  testPathIgnorePatterns: ["/node_modules/", "/build/"]
-})
+  testPathIgnorePatterns: ['/node_modules/', '/build/', '/dist/'],
+});
 
-module.exports = config
+module.exports = config;
